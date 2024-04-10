c2d6135e8dd67acc377830219d1f3fd1dd466d4d
dawid <dawid_sajewski_2011@o2.pl>
2024-04-10 19:55:49 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+function changeLanguage(lang) {
+  var textsToChange = document.querySelectorAll('[data-lang]');
+  textsToChange.forEach(function(elem) {
+    elem.textContent = translations[lang][elem.getAttribute('data-lang')];
+  });
+}
+
+var translations = {
+  'en': {
+    'intro': 'Introduction to the project',
+    'stage1': 'Stage 1: Selection of potential sites for photovoltaic farms',
+    'stage2': 'Stage 2: Selection of potential sites for wind farms',
+    'stage3': 'Stage 3: Solar radiation values for the selected areas',
+    // continue for each piece of text you want to translate
+  },
+  'pl': {
+    'intro': 'Wprowadzenie do projektu',
+    'stage1': 'Etap 1: Selekcja potencjalnych miejsc pod farmy fotowoltaiczne',
+    'stage2': 'Etap 2: Selekcja potencjalnych miejsc pod farmy wiatrowe',
+    'stage3': 'Etap 3: Wartości promieniowania słonecznego dla wyselekcjonowanych obszarów',
+    // the rest of your original Polish text goes here
+  }
+};
+
+
+
+
 function changeTab(evt, tabName) {
     var i, tabcontent, tab;
     tabcontent = document.getElementsByClassName("tabcontent");
